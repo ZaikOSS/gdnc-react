14c071a2999879f183d685c74f8a024f120b5f9f
ZaikOSS <zakariaouadifi3@gmail.com>
2025-01-25 19:36:56 +0100

    fix api

--- src/components/ModuleList.jsx
+++ src/components/ModuleList.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ModuleItem from "./ModuleItem";
-import modules from "../module.json"; // Import the JSON file
 import "./moduleList.css";
 
 const ModuleList = () => {
+  const [modules, setModules] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    // Fetch data from the JSON file URL
+    const fetchModules = async () => {
+      try {
+        const response = await fetch(
+          "https://zaikoss.github.io/api_gdnc_website/module.json"
+        );
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        setModules(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchModules();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="p-6 bg-black text-green-500">Loading modules...</div>
+    );
+  }
+
+  if (error) {
+    return <div className="p-6 bg-black text-red-500">Error: {error}</div>;
+  }
+
   return (
     <div className="p-6 bg-black text-green-500">
       <h2 className="text-2xl font-semibold mb-4 animate-flicker">
