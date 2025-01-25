9cd0edb9c2c3f2cfbbf9093bed49ee60bb67c83a
ZaikOSS <zakariaouadifi3@gmail.com>
2025-01-25 19:50:00 +0100

    fix home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -10,13 +15,18 @@ const App = () => {
   return (
     <Router>
       <div className="flex flex-col min-h-screen bg-black text-white">
+        {/* Navbar is always visible */}
         <Navbar />
         <main className="flex-grow p-4">
           <Routes>
+            {/* Default route to Home */}
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<AboutMe />} />
+            {/* Catch-all route to redirect to Home */}
+            <Route path="*" element={<Navigate to="/" />} />
           </Routes>
         </main>
+        {/* Footer is always visible */}
         <Footer />
       </div>
     </Router>
